7167b4b47db02c2818f9c035fc9b56606f8b8752
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-24 21:06:35 -0200

    Botón flotante - FloatingActionButton

--- contador/lib/src/pages/home_page.dart
+++ contador/lib/src/pages/home_page.dart
@@ -19,6 +19,13 @@ class HomePage extends StatelessWidget{
             Text('0', style: estiloTexto )
           ]
         )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.add ),
+        onPressed: () {
+
+          print('Hola mundo!');
+        },
       )
     );
   }

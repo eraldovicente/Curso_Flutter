15bef0748f478b6db5ec011d29ba9adfec139cdd
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-25 01:44:25 -0200

    Retirando banner de debug

--- contador/lib/src/app.dart
+++ contador/lib/src/app.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build( context ) {
   
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Center(
         // child: HomePage(),
         child: ContadorPage()

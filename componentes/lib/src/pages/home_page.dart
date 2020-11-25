c8b4c4928ec3aa425146fdd941e1e9149096f497
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-25 20:03:15 -0200

    Future Builder

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -16,21 +16,41 @@ class HomePage extends StatelessWidget {
 
   Widget _lista() {
 
-    print( menuProvider.opciones );
+    // print( menuProvider.opciones );
+    // menuProvider.cargarData().then( (opciones) {
+    //   print( '_lista' );
+    //   print( opciones );
+    // });
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot ) {
 
-    return ListView(
-      children: _listaItems(),
+        return ListView(
+          children: _listaItems( snapshot.data ),
+        );
+
+      },
     );
   }
 
-  List<Widget> _listaItems() {
+  List<Widget> _listaItems( List<dynamic> data ) {
 
-    return [
-      ListTile( title: Text('Hola Mundo') ),
-      Divider(),
-      ListTile( title: Text('Hola Mundo') ),
-      Divider(),
-      ListTile( title: Text('Hola Mundo') ),
-    ];
+    final List<Widget> opciones = [];
+
+    data.forEach( (opt) {
+
+      final widgetTemp = ListTile(
+        title: Text( opt['texto'] ),
+        leading: Icon( Icons.account_circle ),  
+        trailing: Icon( Icons.keyboard_arrow_right, color: Colors.blue ),
+        onTap: () {},
+      );
+
+      opciones..add( widgetTemp )
+          ..add( Divider() );
+    });
+
+    return opciones;
   }
 }

797714b44bed236a536b4700aac6360038f0a9d6
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-26 21:28:20 -0200

    Personalizando el estilo de las cards

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -12,7 +12,20 @@ class CardPage extends StatelessWidget {
         children: [
           _cardTipo1(),
           SizedBox(height: 30.0),
-          _cardTipo2()
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
         ]),
     );
   }
@@ -20,6 +33,8 @@ class CardPage extends StatelessWidget {
   Widget _cardTipo1() {
 
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0) ),
       child: Column(
         children: [
           ListTile(
@@ -47,7 +62,8 @@ class CardPage extends StatelessWidget {
 
   Widget _cardTipo2() {
 
-    return Card(
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
       child: Column(
         children: [
           FadeInImage(
@@ -65,6 +81,25 @@ class CardPage extends StatelessWidget {
             child: Text('No tengo idea de que poner')
           )
         ],
+      )
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       )
     );
   }

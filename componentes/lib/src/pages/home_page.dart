ea07167da53ad1882ca6bfeb517a1a98a794a960
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-26 00:07:25 -0200

    Navegar con rutas con nombre

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -50,11 +50,13 @@ class HomePage extends StatelessWidget {
         trailing: Icon( Icons.keyboard_arrow_right, color: Colors.blue ),
         onTap: () {
 
-          final route = MaterialPageRoute(
-            builder: ( context ) => AlertPage()
-          );
+          Navigator.pushNamed(context, opt['ruta'] );
 
-          Navigator.push(context, route);
+          // final route = MaterialPageRoute(
+          //   builder: ( context ) => AlertPage()
+          // );
+
+          // Navigator.push(context, route);          
 
         },
       );

bb67aefabe59aa9234ad5816beea9b625cf5e7e8
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-26 00:36:18 -0200

    Rutas generadas dinamic y archivo de rutas indep

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
  
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
 
 void main() => runApp(MyApp());
  
@@ -14,11 +13,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage()
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/'      : ( BuildContext context ) => HomePage(),
-        'alert'  : ( BuildContext context ) => AlertPage(),
-        'avatar' : ( BuildContext context ) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: ( RouteSettings settings ) {
 
+        print( 'Rutas llamada: ${ settings.name }' );
+
+        return MaterialPageRoute(
+          builder: ( BuildContext context ) => AlertPage()
+        );
       },
     );
   }

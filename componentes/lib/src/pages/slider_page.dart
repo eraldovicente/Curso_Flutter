8ee50ef54197fe6d31733bb443aa8d10afb7d4e4
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-29 00:59:47 -0200

    CheckBox y Switchs

--- componentes/lib/src/pages/slider_page.dart
+++ componentes/lib/src/pages/slider_page.dart
@@ -8,6 +8,7 @@ class SliderPage extends StatefulWidget {
 class _SliderPageState extends State<SliderPage> {
 
   double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,8 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: [
             _crearSlider(),
+            _checkBox(),
+            _crearSwitch(),
             Expanded(
               child: _crearImagem()
             )
@@ -38,10 +41,44 @@ class _SliderPageState extends State<SliderPage> {
       value: _valorSlider,
       min: 10.0,
       max: 400.0,
-      onChanged: ( valor ) {
+      onChanged: ( _bloquearCheck ) ? null : (valor) {
 
         setState(() {
           _valorSlider = valor;
+        });
+      },
+    );
+  }
+
+  Widget _checkBox() {
+
+    // return Checkbox(
+    //   value: _bloquearCheck,
+    //   onChanged: (valor){
+    //     setState(() {
+    //       _bloquearCheck = valor;
+    //     });
+    //   },
+    // );
+
+    return CheckboxListTile(
+      title: Text('Bloquear slider'),
+      value: _bloquearCheck,
+      onChanged: (valor){
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+      title: Text('Bloquear slider'),
+      value: _bloquearCheck,
+      onChanged: (valor){
+        setState(() {
+          _bloquearCheck = valor;
         });
       },
     );

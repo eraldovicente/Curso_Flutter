b08168cccff0c985071de12e07cf518163fd8999
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-29 02:01:22 -0200

    Listview Builder

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -7,6 +7,7 @@ import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
 import 'package:componentes/src/pages/slider_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
 
@@ -18,6 +19,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'animatedContainer' : ( BuildContext context ) => AnimatedContainerPage(),
     'inputs'            : ( BuildContext context ) => InputPage(),
     'slider'            : ( BuildContext context ) => SliderPage(),
+    'list'              : ( BuildContext context ) => ListaPage(),
   };
   
 }

681e457c14ee7d2c2ef52ff0d0702df2808556f8
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-16 21:04:29 -0200

    Crear un swiper de tarjetas

--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Películas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [
+          IconButton(
+            icon: Icon( Icons.search ),
+            onPressed: () {}
+          )
+        ],
+      ),
       body: SafeArea(
-        child: Text('Hola Mundo!!!!!'),      
+        child: Column(
+          children: [
+            _swiperTarjetas()
+          ],
+        ),      
       )
       
+    );
+  }
+
+  Widget _swiperTarjetas() {
+
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: new Swiper(
+          layout: SwiperLayout.STACK,
+          itemWidth: 200.0,
+          itemBuilder: (BuildContext context,int index){
+            return Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          // pagination: new SwiperPagination(),
+          // control: new SwiperControl(),
+        ),
     );
   }
 }

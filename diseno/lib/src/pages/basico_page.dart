114f8341019f99023dfd04f875d3118d597f24f7
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-06 18:02:55 -0300

    Diseno basico - parte 1

--- diseno/lib/src/pages/basico_page.dart
+++ diseno/lib/src/pages/basico_page.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
+
+  final estiloTitulo    = TextStyle( fontSize: 16.0, fontWeight: FontWeight.bold );
+  final estiloSubTitulo = TextStyle( fontSize: 14.0, color: Colors.grey );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Basico Page'),
+        child: Column(
+          children: [
+
+            Image(
+              image: NetworkImage('https://alfapeople.com/br/wp-content/uploads/sites/12/2019/09/alfapeople-brasil-abre-novo-escritorio-recife-767x510@2x.jpeg'),
+            ),
+
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+              child: Row(
+                children: [
+
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Recife - Pernambuco', style: estiloTitulo),
+                        SizedBox( height: 5.0 ),
+                        Text('La magia del Recife', style: estiloSubTitulo)
+                      ],
+                    ),
+                  ),
+
+                  Icon( Icons.star, color: Colors.red, size: 30.0 ),
+                  Text('41', style: TextStyle( fontSize: 20.0 ) )
+                ],
+              ),
+            )
+          ]
+        )
       ),
     );
   }

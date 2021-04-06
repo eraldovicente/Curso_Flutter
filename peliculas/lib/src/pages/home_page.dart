62a71386def290e7859118123d08f2452f107822
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-05 23:01:31 -0300

    Search delegate

--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -21,7 +21,9 @@ class HomePage extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon( Icons.search ),
-            onPressed: () {}
+            onPressed: () {
+              // showSearch();
+            }
           )
         ],
       ),

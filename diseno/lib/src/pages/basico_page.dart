9b4e46adb9b5b3ff097cdd2a6082ddf4e9352433
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-06 22:25:08 -0300

    Diseno basico - parte 2

--- diseno/lib/src/pages/basico_page.dart
+++ diseno/lib/src/pages/basico_page.dart
@@ -12,34 +12,80 @@ class BasicoPage extends StatelessWidget {
         child: Column(
           children: [
 
-            Image(
-              image: NetworkImage('https://alfapeople.com/br/wp-content/uploads/sites/12/2019/09/alfapeople-brasil-abre-novo-escritorio-recife-767x510@2x.jpeg'),
-            ),
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto()
 
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-              child: Row(
-                children: [
-
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Recife - Pernambuco', style: estiloTitulo),
-                        SizedBox( height: 5.0 ),
-                        Text('La magia del Recife', style: estiloSubTitulo)
-                      ],
-                    ),
-                  ),
-
-                  Icon( Icons.star, color: Colors.red, size: 30.0 ),
-                  Text('41', style: TextStyle( fontSize: 20.0 ) )
-                ],
-              ),
-            )
           ]
         )
       ),
     );
   }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage('https://alfapeople.com/br/wp-content/uploads/sites/12/2019/09/alfapeople-brasil-abre-novo-escritorio-recife-767x510@2x.jpeg'),
+    );
+  }
+
+  Widget _crearTitulo() {
+     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      child: Row(
+        children: [
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Recife - Pernambuco', style: estiloTitulo),
+                SizedBox( height: 5.0 ),
+                Text('La magia del Recife', style: estiloSubTitulo)
+              ],
+            ),
+          ),
+
+          Icon( Icons.star, color: Colors.red, size: 30.0 ),
+          Text('41', style: TextStyle( fontSize: 20.0 ) )
+        ],
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+
+        _accion( Icons.call, 'CALL' ),
+        _accion( Icons.near_me, 'ROUTE' ),
+        _accion( Icons.share, 'SHARE' )
+
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: [
+        Icon( icon, color: Colors.blue, size: 40.0 ),
+        SizedBox( height: 5.0 ),
+        Text( texto, style: TextStyle( fontSize: 15.0, color: Colors.blue ) )
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        textAlign: TextAlign.justify
+      ),
+    );
+  }
+
 }

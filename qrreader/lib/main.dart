312440e65da80d6c4b861a506f32c7a110293c8f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-08 14:45:28 -0300

    Inicio del proyecto - QRScanner

---
+++ qrreader/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:qrreader/pages/home_page.dart';
+import 'package:qrreader/pages/mapa_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home': ( _ ) => HomePage(),
+        'mapa': ( _ ) => MapaPage()
+      }
+    );
+  }
+}

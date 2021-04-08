c060d1f765fff98cde364e6e7729ed239cbb380f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-08 17:33:32 -0300

    Tema de la aplicacion

--- qrreader/lib/pages/home_page.dart
+++ qrreader/lib/pages/home_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'package:qrreader/widget/custom_navigatorbar.dart';
+import 'package:qrreader/widget/scan_button.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(
+            icon: Icon( Icons.delete_forever ),
+            onPressed: () {
+
+            },
+          )
+        ],
+      ),
       body: Center(
         child: Text('Home page'),
-      )
+      ),
+      bottomNavigationBar: CustomNavigatorBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }

54590e1b2ffbbe404ca62e9b604b06de57f8d772
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-08 18:12:18 -0300

    Cambiar la pagina dependiendo del tab seleccionado

--- qrreader/lib/pages/home_page.dart
+++ qrreader/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:qrreader/pages/direcciones_page.dart';
 
+import 'package:qrreader/pages/mapas_page.dart';
 import 'package:qrreader/widget/custom_navigatorbar.dart';
 import 'package:qrreader/widget/scan_button.dart';
 
@@ -19,12 +21,31 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text('Home page'),
-      ),
+      body: _HomePageBody(),
       bottomNavigationBar: CustomNavigatorBar(),
       floatingActionButton: ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    // Cambiar para mostrar la pagina respectiva
+    final currentIndex = 1;
+
+    switch( currentIndex ) {
+
+      case 0:
+        return MapasPage();
+
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
   }
 }
